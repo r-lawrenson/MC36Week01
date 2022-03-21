e912d2203973a9f65208abd1721f2879a25a5da4
Rob Lawrenson <roblawrenson@hotmail.co.uk>
2022-03-21 08:51:33 +0000

    Added final activity

--- functions.js
+++ functions.js
@@ -49,3 +49,88 @@
 
 // console.log(addUp(12,3));
 
+// // ACTIVITY 01 ======================================================================
+// // Take this code and turn it into arrow function... syntax:
+
+// console.log("=================code from line 055 below is output=================\n");
+
+// function factorial (n) {
+//     if ((n === 0) || (n === 1)) {
+//         return 1;
+//     }
+//     else {
+//         return (n * factorial(n-1));
+//     }
+// }
+
+// console.log(factorial(33));
+
+// const factorial = (num) => {
+//     if ((num === 0) || (num === 1)) {
+//         return 1;
+//     }
+//     else {
+//         return (num * factorial(num-1));
+//     }
+// }
+
+// console.log(factorial(33));
+
+// // ACTIVITY 02 =====================================================================
+// // Edit the below snippet to include two parameters and a
+// // running order count updated when the function is called:
+
+// console.log("=================code from line 083 below is output=================\n");
+
+// let orderCount = 0;
+
+// const takeOrder = (topping) => {
+//     console.log(`Pizza with ${topping}`);
+//     orderCount++
+// }
+
+// takeOrder("pineapple");
+
+// // 2 parameters
+
+// console.log("=================code from line 096 below is output=================\n");
+
+// let orderCount = 0;
+
+// const takeOrder = (topping1, topping2) => {
+//     orderCount++
+//     console.log(`Your ${orderCount} pizza with ${topping1} and ${topping2}`);
+// }
+
+// takeOrder("pineapple", "peppers");
+
+// // ACTIVITY 03 Cash Machine=========================================================
+// // Cash machine time! Let’s create one that:
+// // Dispenses cash if your pin number is correct and your balance is equal to, 
+// // or more than, the amount you’re trying to withdraw!
+
+// console.log("=================code from line 112 below is output=================\n");
+
+// let accountPin = 1234;
+// let inputPin = 1234;
+// let accBalance = 0;
+
+// const cashMachine = (accountPin, inputPin, withdrawAmount) => {
+//     if (inputPin == accountPin) {
+//         accBalance = Math.round(Math.random()*100);
+//         console.log(accBalance);    
+//         if (withdrawAmount <= accBalance) {
+//             console.log(`Your withdrawal of £${withdrawAmount} has completed and your balance is now £${accBalance - withdrawAmount}`);
+//         }
+//         else {
+//             console.log(`Nope! Sorry! That's not going to happen, your balance is £${accBalance} and the £${withdrawAmount} you want to withdraw is more than your available balance.`);
+//         }
+//     }
+//     else {
+//         console.log("please enter your correct PIN number");
+//     }
+// };
+
+// cashMachine(accountPin, inputPin, 50);
+
+// // need to figure out inputs...
